a3646d0980525f4f1ee55f4bc0382364056f205c
envyworen <envyworen@gmail.com>
2020-12-14 09:20:24 +0100

    Ajout js V2

--- JS/main.js
+++ JS/main.js
@@ -1,28 +1,4 @@
-var button = document.querySelector(".btn");
-var content = document.querySelector(".content");
 
-button.addEventListener("click", function () {
-  var valeur = document.querySelector(".input").value;
-  var supr = document.querySelectorAll(".texte");
-  for (let i = 0; i < supr.length; i++) {
-    supr[i].remove();
-  }
-  if (valeur !== "") {
-    for (let i = 0; i < valeur; i++) {
-      var pp = document.createElement("p");
-      pp.className = "texte";
-      pp.innerText = "wsh la zone";
-      content.appendChild(pp);
-    }
-  }
-});
-
-/* parag 1 */
-
-let paragraphe = document.querySelector(".parag");
-let modif = document.querySelector(".modifier");
-let valider = document.querySelector(".valider");
-let cadre = paragraphe.innerText;
 /*
 modif.addEventListener("click", function () {{
   valider.style.display = "inline";
