4cd8af47f69d1e81ace24786de02da95ecbfa11d
maxime-kupke <maxime.kupke@gmail.com>
2020-12-15 12:15:26 +0100

    Modification de scss, ajout de pages etc..

---
+++ JS/script.js
@@ -0,0 +1,15 @@
+let elt = document.querySelector(".text");
+let elt2 = document.querySelector("#canvas");
+document.getElementById("button").addEventListener("click", function () {
+  elt2.style.opacity = "0";
+  elt.style.opacity = "0";
+  elt.style.transform = "scale(0)";
+});
+
+let test = document.querySelector(".zoom");
+elt.addEventListener("transitionend", () => {
+  document.querySelector(".index").style.display = "none";
+  test.style.display = "inline";
+  test.style.opacity = "1";
+  test.style.transform = "scale(1)";
+});
