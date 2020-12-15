2b53c12a6954b9c6107b4e14f83ee592ee241393
envyworen <envyworen@gmail.com>
2020-12-15 09:21:31 +0100

    Maj user

--- include/CompoInsert.php
+++ include/CompoInsert.php
@@ -2,7 +2,6 @@
 // Inserer une composition
 function compo_insert($dbh){
     if (isset($_POST['comp'])){
-        var_dump($_POST);
         $st = $_POST['st_comp'];
         $text = $_POST['texte_comp'];
         $autcomp = $_POST['auteur_comp'];
