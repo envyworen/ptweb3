3ca59797221c17f5e093227f2467f2c483acf394
maxime-kupke <maxime.kupke@gmail.com>
2020-12-15 23:32:42 +0100

    yes

--- JS/script.js
+++ JS/script.js
@@ -1,11 +1,12 @@
 let maison = document.querySelector(".maison");
+let blocktest = document.querySelector(".blocktest");
 maison.style.display = "none";
 let elt = document.querySelector(".text");
 let elt2 = document.querySelector("#canvas");
 document.getElementById("button").addEventListener("click", function () {
   elt2.style.opacity = "0";
   elt.style.opacity = "0";
-  maison.style.opacity = "0";
+  blocktest.style.opacity = "0";
   elt.style.transform = "scale(0)";
 });
 
@@ -13,7 +14,7 @@ document.getElementById("button").addEventListener("click", function () {
 let test = document.querySelector(".zoom");
 elt.addEventListener("transitionend", () => {
   maison.style.display = "flex";
-  maison.style.opacity = "1";
+  blocktest.style.opacity = "1";
   document.querySelector(".index").style.display = "none";
   test.style.display = "inline";
   test.style.opacity = "1";
