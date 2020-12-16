78d922173686119bad3cbd7c46800e39bbd13349
maxime-kupke <maxime.kupke@gmail.com>
2020-12-16 17:39:43 +0100

    js pour article

--- JS/article.js
+++ JS/article.js
@@ -5,15 +5,32 @@ articles.forEach(function(article) {
       return Math.floor(Math.random() * (max - min)) + min;
     }
     
+    let link = createElement("a");
+    link.classList.add("boule");
+    link.style.marginTop = getRandomInt(20, 230)+"px";
+    link.style.marginLeft = getRandomInt(20, 230)+"px";
+
+    let point = document.createElement("span");
+    link.appendChild(point);
+
+    let description = document.createElement("p");
+    point.appendChild(description);
+
     let tous = document.querySelector(".tous");
     let block = document.createElement("div");
     block.style.width = "250px";
     block.style.height = "250px";
     block.style.backgroundColor = "green";
+    block.appendChild(link);
     tous.appendChild(block);
     
-    let point = document.createElement("span");
-    point.style.marginTop = getRandomInt(0, 200)+"px";
-    point.style.marginLeft = getRandomInt(0, 200)+"px";
-    block.appendChild(point);
   });
+
+let liens = querySelectorAll(".boule");
+
+for (var i = 0; i < liens.length; i++) {
+  liens[0].href = "https://violence.caquelin.etu.mmi-unistra.fr/detail-inconcevable-12";
+  liens[1].href = "https://violence.caquelin.etu.mmi-unistra.fr/detail-inconcevable-14";
+  liens[2].href = "https://violence.caquelin.etu.mmi-unistra.fr/detail-inconcevable-15";
+  liens[3].href = "https://violence.caquelin.etu.mmi-unistra.fr/detail-inconcevable-16";
+}
