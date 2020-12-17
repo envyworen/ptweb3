a6a30b72ef9e0025791a40961bf946e3a343b89c
envyworen <envyworen@gmail.com>
2020-12-17 19:31:22 +0100

    next page

--- JS/justifier.js
+++ JS/justifier.js
@@ -37,8 +37,8 @@ articles.forEach(function(article) {
   tous.appendChild(svg);
 
   let ligne = document.createElement("line");
-  ligne.setAttribute("x1", "0");
-  ligne.setAttribute("y1", "0");
+  ligne.setAttribute("x1", 0);
+  ligne.setAttribute("y1", 0);
   ligne.setAttribute("x2", eX/16);
   ligne.setAttribute("y2", eY/16);
   ligne.style.stroke = "rgb(255,0,0)";
