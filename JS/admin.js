2b76a08a48bbb843dca99d3c4cbc2f59d8900cff
envyworen <envyworen@gmail.com>
2020-12-18 14:07:58 +0100

    bouton co

--- JS/admin.js
+++ JS/admin.js
@@ -4,26 +4,31 @@ tabjson.forEach(function(drop) {
     let menu = document.createElement('a');
     let mi = document.createElement('li');
     console.log(liste);
-    mi.appendChild(menu);
+  
     menu.innerText = drop.h1_art;
     if (drop.titre_art == 'surveillance') {
         menu.href = "detail-" + drop.titre_art + "-" + drop.id_art;
+          mi.appendChild(menu);
         liste[0].appendChild(mi);
     }
       if (drop.titre_art == 'force') {
         menu.href = "detail-" + drop.titre_art + "-" + drop.id_art;
+          mi.appendChild(menu);
         liste[4].appendChild(mi);
     }
       if (drop.titre_art == 'profils') {
         menu.href = "detail-" + drop.titre_art + "-" + drop.id_art;
+          mi.appendChild(menu);
         liste[2].appendChild(mi);
     }
       if (drop.titre_art == 'silence') {
         menu.href = "detail-" + drop.titre_art + "-" + drop.id_art;
+          mi.appendChild(menu);
         liste[1].appendChild(mi);
     }
           if (drop.titre_art == 'inconcevable') {
         menu.href = "detail-" + drop.titre_art + "-" + drop.id_art;
+          mi.appendChild(menu);
         liste[3].appendChild(mi);
     }
 });
