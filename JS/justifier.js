d50d27137ae26bdbd7c22c49503bc4d7d3414fc1
maxime-kupke <maxime.kupke@gmail.com>
2020-12-18 11:17:44 +0100

    création de trait

--- JS/justifier.js
+++ JS/justifier.js
@@ -41,6 +41,8 @@ articles.forEach(function(article) {
   ligne.setAttribute("y1", "250");
   ligne.setAttribute("x2", eX);
   ligne.setAttribute("y2", eY);
+  ligne.setAttribute("version", "1.1");
+  ligne.setAttribute("xmins", "http://www.w3.org/2000/svg");
   ligne.style.stroke = "rgb(255,0,0)";
   ligne.style.strokeWidth = "4px";
   svg.appendChild(ligne);
